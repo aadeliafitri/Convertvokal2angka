f52ed3f9cdaf16ee22a6d64bd9d7f1ccd0cc7231
WINDOWS 10 <WINDOWS 10@DESKTOP-EO5EC5V>
2018-08-14 21:42:57 +0700

    Initial Commit

---
+++ src/jobsheet11/Latihan2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet11;
+import java.util.Scanner;
+public class Latihan2 {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+    public static void main(String[] args) {
+        String identitas = "Adelia Fitri Kristanti / XRPL2 / 02";
+        
+        tampilJudul(identitas);
+    }
+}
